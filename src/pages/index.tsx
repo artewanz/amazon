ddd5736e611bc50c1d70f391a395c715d29ad5c3
Artem Anzin <anzinartix@gmail.com>
2022-10-10 23:39:51 +0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '@src/styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Amazon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
